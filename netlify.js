383a4b919f4d5ee4b770e3ef23ceb235c7a74beb
Remy Sharp <remy@remysharp.com>
2022-09-12 17:35:28 +0100

    use force on netlify redirect

--- netlify.js
+++ netlify.js
@@ -9,7 +9,7 @@ fs.readdir(__dirname, (error, files) => {
       return f.replace(/^index-(.*)\.html/, '$1');
     })
     .map(lang => {
-      return `https://${lang}.${domain} /index-${lang}.html 200\nhttp://${lang}.${domain} /index-${lang}.html 200`;
+      return `https://${lang}.${domain} /index-${lang}.html 200!\nhttp://${lang}.${domain} /index-${lang}.html 200!`;
     });
 
   fs.writeFileSync(__dirname + '/_redirects', lines.join('\n'), 'utf8');
